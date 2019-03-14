3778130fde917b239b872d5795a8c9804ba18291
Cy Allen Scott <cy.a.scott@live.com>
2019-03-13 23:49:48 -0400

    setup vs solution

---
+++ src/ClientApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClientApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
